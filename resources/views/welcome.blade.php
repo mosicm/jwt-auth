e1282d50db1a6ac2b8a7c34417ddcd86b06dd9f0
Milos Mosic <milos.mosic@quantox.com>
2018-12-27 13:00:07 +0100

    3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
                     Computer 1 did something new!!!
                 </div>
                 <div class="title m-b-md">
-                    Computer 2 new Model Post and new Migration!
+                    Computer 3 new Model Post and new Migration!
                 </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
