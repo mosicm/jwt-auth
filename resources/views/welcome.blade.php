4755a6e5dce48201675231269a28db6e4b0c484b
Milos Mosic <milos.mosic@quantox.com>
2018-12-27 13:53:28 +0100

    laracasts master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                 </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation masters</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laracasts.com">Laracasts master</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
